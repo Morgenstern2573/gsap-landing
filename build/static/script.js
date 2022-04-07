68930509cd610969f649ecbb943f9de5eb09130f
Bosun Akinyemi <azurnwa.p@gmail.com>
2022-04-07 23:51:34 +0100

    create responsive animation

--- build/static/script.js
+++ build/static/script.js
@@ -1,4 +1,6 @@
 let timeLine = gsap.timeline({defaults: {duration: 1}})
+const isMobile = !(window.matchMedia('(min-width: 768px)').matches)
+
 timeLine.to('.pre-loader > .overlay', {top: '75%'})
 timeLine.to('.pre-loader > .overlay', {top: '50%', delay: 0.5})
 timeLine.to('.pre-loader > .overlay', {top: '25%', delay: 0.5})
@@ -7,5 +9,10 @@ timeLine.to('.pre-loader', {width: '80vw', left:0, top: '50%'})
 timeLine.fromTo('nav', {y:-100}, {y:0, opacity:1})
 timeLine.fromTo('.first-row .bold-text', {y:100}, {y:0, opacity:1}, "<")
 timeLine.fromTo('.second-row .bold-text', {y:100}, {y:0, opacity:1, delay:0.5})
-timeLine.fromTo('.first-row .copy', {y:100}, {y:0, opacity:1, delay:0.5})
-timeLine.set('.round-text', {opacity:1, delay:0.5})
+if (!isMobile) {
+    timeLine.fromTo('.first-row .copy', {y:100}, {y:0, opacity:1, delay:0.5})
+    timeLine.set('.round-text', {opacity:1, delay:0.5})
+} else {
+    timeLine.fromTo('.mobile-row .copy', {y:100}, {y:0, opacity:1, delay:0.5})
+    timeLine.fromTo('.mobile-row .cta', {y:100}, {y:0, opacity:1, delay:0.5})
+}
